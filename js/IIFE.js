e1c4b89e04ec3cd0aa203df1a114623f1b52b2fe
divyanshu-png <thedndivyanshu27@gmail.com>
2026-07-13 16:20:50 +0530

    Advanced js Practiced, files added in the JS folder

--- js/IIFE.js
+++ js/IIFE.js
@@ -3,3 +3,32 @@
 
 //its a function that invokes itself when defined.
 
+(function (name) {
+  let text = "Hello " + name;
+  return text; 
+})("John Doe");
+
+//arrow notation of IIFE
+(() => {
+  let text = "Hello! I called myself.";
+})();
+
+//NAMED IIFE
+(function greet() {
+  let text = "Hello! I called myself.";
+})();
+
+greet();
+
+
+//IIFE as a Module (Private Variables)
+const counter = (function () {
+  let value = 0;
+  return {
+    increment() { value++; },
+    get() { return value; }
+  };
+})();
+
+counter.increment();
+let x = counter.get();
---
+++ js/closures.js
@@ -0,0 +1,19 @@
+//Closures make it possible for a function to have "private" variables.
+
+//A closure is created when a function remembers the variables from its outer scope, even after the outer function has finished executing.
+
+function myCounter() {
+  let counter = 0;
+  return function() {
+    counter++;
+    return counter;
+  };
+}
+const add = myCounter();
+add();
+add();
+add();
+
+// the counter is now 3
+
+// a b b b a b b b b a b
